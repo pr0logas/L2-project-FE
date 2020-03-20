0642788f7e71c2fd360c789895d156f271dd7e7e
Mister Nemo <miister.nemo@gmail.com>
2020-03-20 18:07:12 +0200

    fix func

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-﻿$(document).ready(function() { 
+﻿$(document).ready(function() 
+{ 
+	afterUpdate();
 
 	getPage('home.html');
 
-	$('a').click(function(event){
-	    getPage(this);
-	    event.preventDefault();
-	});
-
 	changeOnline('#online');
 });
+
+function afterUpdate() 
+{
+	$('a').click(function(event){
+		var href = $(this).attr('href');
+		if(href.indexOf('.html') < 0)
+			return;
+		event.preventDefault();
+	    getPage(this);
+	});
+
+	$('form').submit(function(event){
+		event.preventDefault();
+	    submitForm(this);
+	});
+}
