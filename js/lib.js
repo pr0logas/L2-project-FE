862e8890a47a6527d47dcefc8299a1eff0463e2e
Mister Nemo <miister.nemo@gmail.com>
2020-03-20 17:18:58 +0200

    append func

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,16 @@
-﻿function getPage(el) 
+﻿link = {
+	online: 'https://l2-corona-api.adeptio.cc/apiv1/getOnline',
+}
+
+function getPage(el) 
 {
 	var link = getLink(el);
 
 	$.get(link, function( data ) {
 	  putContent(data);
 	});
+
+	changeUrl(link);
 }
 
 function getLink(el) 
@@ -19,5 +25,33 @@ function getLink(el)
 
 function putContent(data) 
 {
-	$('#page').html(data);
+	removeChilds('main.content', ['media-block','feature-block']);
+	$('main.content').prepend(data);
+}
+
+function removeChilds(el, leave) 
+{
+	$(el).children().each(function () {
+		var attr = $(this).attr('class');
+    	if(leave.indexOf(attr) < 0)
+    		$(this).remove();
+	});
+}
+
+function changeUrl(link) 
+{
+	//window.history.pushState('page2', 'Title', link);
+}
+
+function changeOnline(el) 
+{
+	var online = "<span>Online</span>";
+	var offline = "<span>Offline</span>";
+	var end = " Players";
+
+	$(el).html(online + 0 + end);
+
+	$.get(link.online, function( data ) {
+		$(el).html(online + data + end);
+	});
 }
