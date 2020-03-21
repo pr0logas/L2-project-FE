4dc22b0acec7d91c9299cb6e7e7a371478bafe31
Mister Nemo <miister.nemo@gmail.com>
2020-03-21 18:48:53 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -12,13 +12,13 @@
 function afterUpdate() 
 {
 	$('a').click(function(event){
+		var href = $(this).attr('href');
+		if(href.indexOf('.html') < 0)
+			return;
 		event.preventDefault();
 		if($(this).attr("loading") === "true")
 			return;
-		var href = $(this).attr('href');
 		$(this).attr("loading", "true");
-		if(href.indexOf('.html') < 0)
-			return;
 	    getPage(this,true);
 	});
 
